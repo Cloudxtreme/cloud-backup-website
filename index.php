82984ea6f214d37be8ce1c235633f36842ae68f0
Ashish Patel <Ashish Patel>
2016-04-07 01:14:28 +0600

    fix image problem

--- index.php
+++ index.php
@@ -22,21 +22,21 @@
 
             <div  class="col s12 m4 l4">
                 <div class="center promo promo-example">
-                    <i class="mdi-image-flash-on"></i>
+                    <i class="mdi-hardware-security"></i>
                     <h5 class="promo-caption">Fingerprint Security</h5>
                     <p class="light center">Our app has fingerprint security which makes provides easy and fast security to your photos.</p>
                 </div>
             </div>
             <div class="col s12 m4 l4"> 
                 <div class="center promo promo-example">
-                    <i class="mdi-social-group"></i>
+                    <i class="mdi-file-cloud"></i>
                     <h5 class="promo-caption">Cloud Backup</h5>
                     <p class="light center">Users can upload their photos to the cloud when if they are running low on internal storage</p>
                 </div>
             </div>
             <div class="col s12 m4 l4">
                 <div class="center promo promo-example">
-                    <i class="mdi-hardware-desktop-windows"></i>
+                    <i class="mdi-action-android"></i>
                     <h5 class="promo-caption">Android App</h5>
                     <p class="light center">Our Android app is available on google play market. It has material design and size of app is really small</p>
                 </div>
