1a4ea728ca77f41fa105a391fb9bf5d0300b416e
Ashish Patel <Ashish Patel>
2016-04-07 00:51:25 +0600

    fix photos navigation link

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	/* Check Login form submitted */	
 	if(isset($_POST['Submit'])){
 		/* Define username and associated password array */
-		$logins = array('Alex' => '123456','username1' => 'password1','username2' => 'password2');
+		$logins = array('admin' => 'admin','username1' => 'password1','username2' => 'password2');
 		
 		/* Check and assign submitted Username and Password to new variable */
 		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
@@ -13,7 +13,7 @@
 		if (isset($logins[$Username]) && $logins[$Username] == $Password){
 			/* Success: Set session variables and redirect to Protected page  */
 			$_SESSION['UserData']['Username']=$logins[$Username];
-			header("location:home.php");
+			header("location:photos.php");
 			exit;
 		} else {
 			/*Unsuccessful attempt: Set error message */
